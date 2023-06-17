b3649710ed9e656083b707b72c02d73bd2c267b3
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-17 12:36:16 +0300

    reverse-words assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Good Luck 💪🏾
+
+
+function reverseWords(sentence) {
+  return sentence.split(' ').reverse().join(' ');
+}
+
+var sentence = 'My name is Ayaan Khalif';
+var reversedSentence = reverseWords(sentence);
+console.log(reversedSentence);
+
+
